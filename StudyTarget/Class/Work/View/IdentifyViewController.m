66e09035d4b0a3a2c9a544947c8ee3b09b7f3289
mengyang <mengyang@hillinsight.com>
2016-12-14 21:21:22 +0800

    添加二维码demo

---
+++ StudyTarget/Class/Work/View/IdentifyViewController.m
@@ -0,0 +1,37 @@
+//
+//  IdentifyViewController.m
+//  StudyTarget
+//
+//  Created by 杨梦 on 2016/12/14.
+//  Copyright © 2016年 com.hillhouse.news. All rights reserved.
+//
+
+#import "IdentifyViewController.h"
+
+@interface IdentifyViewController ()
+
+@end
+
+@implementation IdentifyViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
---
+++ StudyTarget/Tools/WinTreasureConfig.h
@@ -0,0 +1,25 @@
+//
+//  WinTreasureConfig.h
+//  WinTreasure
+//
+//  Created by Apple on 16/5/31.
+//  Copyright © 2016年 linitial. All rights reserved.
+//
+
+#ifndef WinTreasureConfig_h
+#define WinTreasureConfig_h
+
+#pragma mark - base
+#import "BaseViewController.h"
+#import "BaseNavigationController.h"
+#pragma mark - system
+#import "WTSystemInfo.h"
+
+
+#pragma mark - common
+#import <YYKit.h>
+
+#pragma mark - category
+
+
+#endif /* WinTresureConfig_h */
